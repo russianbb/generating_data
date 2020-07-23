509972c9d55efc213ca94e96165b16a7ff9604b0
Bruno Borges Russian <bruno.russian.i@gmail.com>
2020-07-23 18:33:39 -0300

    All up for backup

---
+++ eq_explore_data.py
@@ -0,0 +1,17 @@
+import json
+
+#Explore the structure of the data
+filename = 'data_earthquake/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
